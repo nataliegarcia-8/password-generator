96f689fb8ddc3e0b3aa21821b73378aca4bd435d
Natalie Garcia <gnatalie.booking@gmail.com>
2020-06-26 18:48:01 -0400

    adding confirms to trigger character vars, start on for loop- not working

--- script.js
+++ script.js
@@ -6,25 +6,46 @@ var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var num = "0123456789";
 var sym = "!@#$%^&*=-_";
 
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+  
   passwordText.value = password;
-
+  
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
 
+function generatePassword() {
+  var generate = "";
+  
+
 var length = prompt("How long would you like your password to be?");
 
-if(length >= 8 && length <= 128) {
 
+if (length >= 8 && length <= 128) {
+  
   var confirmLower = confirm("Do you want lowercase letters in your password?");
   var confirmUpper = confirm("Do you want uppercase letters in your password?");
   var confirmNum = confirm("Do you want numbers in your password?");
   var confirmSym = confirm("Do you want special characters in your password?");
+}
+
+if(confirmLower === true)
+{generate += lower};
+if(confirmUpper === true) 
+{generate += upper};
+if(confirmNum === true)  
+{generate += num};
+if(confirmSym === true) 
+{generate += sym};
+
+
+  for (var i = 0; i < length; i++ ) {
+    var thePassword = generate.charAt(Math.floor(Math.random() * generate[i]))
+
 }
