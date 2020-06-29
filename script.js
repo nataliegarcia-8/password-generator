24c3cb4235d47b49de8e823aec23c574d26dadbe
Natalie Garcia <gnatalie.booking@gmail.com>
2020-06-29 18:38:29 -0400

    fixed for loop, added else statements if number or confirming didn't match,added returnChar function to put in for loop

--- script.js
+++ script.js
@@ -7,45 +7,75 @@ var num = "0123456789";
 var sym = "!@#$%^&*=-_";
 
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-  
-  passwordText.value = password;
-  
-}
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
 
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// function to generate password
 function generatePassword() {
   var generate = "";
-  
+  var password = "";
 
-var length = prompt("How long would you like your password to be?");
+  var length = prompt("How long would you like your password to be?");
 
+  length = parseInt(length);
 
-if (length >= 8 && length <= 128) {
-  
-  var confirmLower = confirm("Do you want lowercase letters in your password?");
-  var confirmUpper = confirm("Do you want uppercase letters in your password?");
-  var confirmNum = confirm("Do you want numbers in your password?");
-  var confirmSym = confirm("Do you want special characters in your password?");
-}
-
-if(confirmLower === true)
-{generate += lower};
-if(confirmUpper === true) 
-{generate += upper};
-if(confirmNum === true)  
-{generate += num};
-if(confirmSym === true) 
-{generate += sym};
-
-
-  for (var i = 0; i < length; i++ ) {
-    var thePassword = generate.charAt(Math.floor(Math.random() * generate[i]))
+  if (length >= 8 && length <= 128) {
+    var confirmLower = confirm("Do you want lowercase letters in your password?");
+    var confirmUpper = confirm("Do you want uppercase letters in your password?");
+    var confirmNum = confirm("Do you want numbers in your password?");
+    var confirmSym = confirm("Do you want special characters in your password?");
+  } else {
+    var resume = confirm("Must be numerical value between 8 and 128, or exit")
+    if (resume) {
+      return generatePassword();
+    }
+    return ''
   }
+
+  if (!confirmLower && !confirmUpper && !confirmNum && !confirmSym) {
+    var resume = confirm("Must pick atleast one option, or exit")
+    if (resume) {
+      return generatePassword();
+    }
+    return ''
+  }
+
+  if (confirmLower === true) {
+    generate += lower
+    password += returnChar(lower)
+  };
+  if (confirmUpper === true) {
+    generate += upper
+    password += returnChar(upper)
+  };
+  if (confirmNum === true) {
+    generate += num
+    password += returnChar(num)
+  };
+  if (confirmSym === true) {
+    generate += sym
+    password += returnChar(sym)
+  };
+
+
+  for (var i = password.length; i < length; i++) {
+    password += returnChar(generate);
+  }
+
+  return password;
 }
+
+function returnChar(str) {
+  return str[Math.floor(Math.random() * str.length)]
+}
+
